474a130720183bcbf882e64d1f0b895b9a170ae0
deepakinnoblitz <deepak.innoblitz@gmail.com>
2026-08-25 06:04:05 +0000

    Changes in Hooks

--- company/hooks.py
+++ company/hooks.py
@@ -48,7 +48,7 @@ app_include_js = [
     # "/assets/company/js/sales_pipeline.js",
 ]
 
-app_include_css = "/assets/company/css/custom.css?v=17"
+app_include_css = "/assets/company/css/custom.css?v=18"
 
 
 doc_events = {
